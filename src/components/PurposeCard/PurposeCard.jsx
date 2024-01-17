e5261d527e747f3c8e0358f4eae85d00325e0bb9
aymanhawk <ahaque2002@gmail.com>
2024-01-16 23:28:04 -0500

    edit: removed purpose card

--- src/components/PurposeCard/PurposeCard.jsx
+++ src/components/PurposeCard/PurposeCard.jsx
@@ -3,19 +3,26 @@ import { styled } from '@mui/system';
 
 const StyledComponents = {
     Card: styled(Card)({
-        backgroundColor: '#1DB954',
-        height:'100vh',
-        width:'600px'
-
+      backgroundColor: '#2DBC58',
+      height: '100vh',
+      width: '600px',
+      padding: '3rem', // Adjust padding as needed
+  
+      // Media Query for Mobile
+      '@media (max-width: 600px)': {
+        width: '100%', // Takes up the full width on mobile
+        padding: '3rem', // Adjust padding as needed for mobile
+      },
     }),
   };
-
-function PurposeCard(){
-    return(
-        <StyledComponents.Card>
-            Hey There 👋🏼
-        </StyledComponents.Card>
-    )
-}
-
-export default PurposeCard;
+  
+  function PurposeCard() {
+    return (
+      <StyledComponents.Card>
+        Hey There 👋🏼
+      </StyledComponents.Card>
+    );
+  }
+  
+  export default PurposeCard;
+  
