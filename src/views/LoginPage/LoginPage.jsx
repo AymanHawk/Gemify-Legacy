750ecb79cb2513e31fc540e31525aeb37fb41ab6
aymanhawk <ahaque2002@gmail.com>
2024-01-31 19:33:14 -0500

    revert: reverted redirect URL

--- src/views/LoginPage/LoginPage.jsx
+++ src/views/LoginPage/LoginPage.jsx
@@ -40,7 +40,7 @@ function LoginPage({}) {
   
   const handleClick = () => {
       const CLIENT_ID = "d1ca8443abd34bbe8d5b33168e16ed54"
-      const REDIRECT_URI = "http://aymanhaque.com" // http://localhost:3000
+      const REDIRECT_URI = "http://localhost:3000" // http://localhost:3000
       const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
       const SCOPE = [
           'user-read-playback-position',
